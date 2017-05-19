3714bd01a3433677427b436f45bd7852621731bd
Jaclyn <jaclyn.d.touchstone@gmail.com>
2017-05-18 22:39:05 -0500

    added waypoints to reveal page sections, project items on scroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+	constructor() {
+		this.itemsToReveal = $(".project-item--up");
+		this.hideInitially();
+		this.createWaypoints();
+	}
+
+	hideInitially() {
+		this.itemsToReveal.addClass("reveal-item__up");
+	}
+
+	createWaypoints() {
+		this.itemsToReveal.each(function() {
+			var currentItem = this;
+			new Waypoint({
+				element: currentItem,
+				handler: function() {
+					$(currentItem).addClass("reveal-item__up--is-visible");
+				},
+				offset: "75%"
+			});
+		});
+	}
+}
+
+export default RevealOnScroll;
