6731d7085500a1be0a6340ac8efaeaa4b278611f
Jaclyn <jaclyn.d.touchstone@gmail.com>
2017-05-26 11:48:03 -0500

    changed intro vid to shorter video, removed project toggle due to intro animation conflict--fix later, switched order of video and logo animation in intro

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import ProjectItem from './modules/ProjectItem';
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
-var projectItem = new ProjectItem();
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -10391,54 +10391,7 @@ var _jquery = __webpack_require__(0);
 
 var _jquery2 = _interopRequireDefault(_jquery);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var ProjectItem = function () {
-	function ProjectItem() {
-		_classCallCheck(this, ProjectItem);
-
-		this.projectTitle = (0, _jquery2.default)(".project-item__item-title");
-		this.projectDesc = (0, _jquery2.default)(".project-item__desc");
-		this.events();
-	}
-
-	_createClass(ProjectItem, [{
-		key: "events",
-		value: function events() {
-			this.projectTitle.click(this.toggleTheDesc.bind(this));
-		}
-	}, {
-		key: "toggleTheDesc",
-		value: function toggleTheDesc() {
-			this.projectDesc.toggleClass("project-item__desc--is-visible");
-		}
-	}]);
-
-	return ProjectItem;
-}();
-
-exports.default = ProjectItem;
-
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _jquery = __webpack_require__(0);
-
-var _jquery2 = _interopRequireDefault(_jquery);
-
-var _noframework = __webpack_require__(5);
+var _noframework = __webpack_require__(4);
 
 var _noframework2 = _interopRequireDefault(_noframework);
 
@@ -10482,7 +10435,7 @@ var RevealOnScroll = function () {
 exports.default = RevealOnScroll;
 
 /***/ }),
-/* 4 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -10492,22 +10445,17 @@ var _MobileMenu = __webpack_require__(1);
 
 var _MobileMenu2 = _interopRequireDefault(_MobileMenu);
 
-var _RevealOnScroll = __webpack_require__(3);
+var _RevealOnScroll = __webpack_require__(2);
 
 var _RevealOnScroll2 = _interopRequireDefault(_RevealOnScroll);
-
-var _ProjectItem = __webpack_require__(2);
-
-var _ProjectItem2 = _interopRequireDefault(_ProjectItem);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mobileMenu = new _MobileMenu2.default();
 var revealOnScroll = new _RevealOnScroll2.default();
-var projectItem = new _ProjectItem2.default();
 
 /***/ }),
-/* 5 */
+/* 4 */
 /***/ (function(module, exports) {
 
 /*!
